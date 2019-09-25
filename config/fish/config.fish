df6c18990a19b7cef4d4d0730fe25a5c8223064a
Yù-Jǐn Lín <abc1236762@outlook.com>
2019-09-25 22:42:19 +0800

    Add files via upload

---
+++ config/fish/config.fish
@@ -0,0 +1,8 @@
+function fish_greeting
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/user/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
